c9f7fa9f52249d187c61e457f248babb0f588819
Cyrill Püntener <wp99cp@gmx.net>
2022-12-06 08:40:23 +0100

    solve tasks of day 6

---
+++ day-06-part-1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <deque>
+#include <set>
+
+
+bool is_unique(std::deque<char> &deque) {
+
+
+    std::set<char> set(deque.begin(), deque.end());
+    return set.size() == deque.size();
+
+}
+
+
+int main(int argc, char **argv) {
+
+    char c;
+    std::deque<char> history;
+
+    for(int pos = 0; std::cin >> c; pos++) {
+
+        history.push_back(c);
+        if (history.size() > 4) {
+            history.pop_front();
+
+            if(is_unique(history)) {
+                std::cout << "Unique found at " << pos + 1 << std::endl;
+                return 0;
+            }
+
+        }
+
+
+    }
+
+    return 0;
+
+}
